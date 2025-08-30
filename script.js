62e606dc660f2b2ef947f99fc271a3a207bc4547
Fabio <filoniym@gmail.com>
2025-08-30 12:02:47 +0700

    Версия 3 после правок из коментов

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
-document.getElementById('loadButton').addEventListener('click', () => {
-      const listElement = document.getElementById('characterList');
-      listElement.innerHTML = '';
+async function fetchCharacters() {
+  try {
+    const response = await fetch('https://rickandmortyapi.com/api/character');
+    const data = await response.json();
+    return data.results.slice(0, 20);
+  } catch (error) {
+    console.error('Ошибка при получении персонажей:', error);
+    return [];
+  }
+}
 
-fetch('https://rickandmortyapi.com/api/character')
-      .then(response => response.json())
-      .then(data => {
-       const characters = data.results.slice(0, 20);
-        console.log("Первые 20 персонажей:", characters);
-        const listElement = document.getElementById('characterList');
-        characters.forEach(character => {
-          const li = document.createElement('li');
-          li.textContent = character.name;
-          listElement.appendChild(li);
-        });
-      })
-      .catch(error => {
-        console.error('Ошибка', error);
-      });
-      });
+function renderCharacters(characters) {
+  const listElement = document.querySelector('.characterList');
+  listElement.innerHTML = ''; 
+    characters.forEach(character => {
+    const li = document.createElement('li');
+    li.textContent = character.name;
+    listElement.appendChild(li);
+  });
+}
+
+const handleLoadButtonClick = async () => {
+  const characters = await fetchCharacters();
+  renderCharacters(characters);
+};
+const loadButton = document.querySelector('.loadButton');
+loadButton.addEventListener('click', handleLoadButtonClick);
